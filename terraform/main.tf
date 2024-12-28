24c86de291f730065171e828187d9903910b2a2d
sheetal-nain <sheetalnain17@gmail.com>
2024-12-28 20:42:57 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -214,6 +214,50 @@ terraform {
     encrypt = true
   }
 }
+data "aws_vpc" "default" {
+  filter {
+    name   = "isDefault"
+    values = ["true"]
+  }
+}
+
+################### VPC Peering Connection ###################
+
+resource "aws_vpc_peering_connection" "Redis_to_default" {
+  vpc_id        = aws_vpc.VPC.id
+  peer_vpc_id   = data.aws_vpc.default.id
+  auto_accept   = true
+
+  tags = {
+    Name = "Redis-to-default-peering"
+  }
+}
+
+################### Routes for Tool VPC ###################
+
+resource "aws_route" "tool_to_default_public" {
+  route_table_id            = aws_route_table.Public_rt.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.Redis_to_default.id
+}
+
+resource "aws_route" "tool_to_default_private" {
+  route_table_id            = aws_route_table.Database_rt.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.Redis_to_default.id
+}
+
+################### Default VPC Route Table Update ###################
+
+data "aws_route_table" "default" {
+  vpc_id = var.default_vpc_id
+}
+
+resource "aws_route" "default_to_tool" {
+  route_table_id            = data.aws_route_table.default.id
+  destination_cidr_block    = aws_vpc.Redis.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.Redis_to_default.id
+}
 
 
 
